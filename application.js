4904bcf670d72a7081f7f58d9008c100f7da1323
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-12 11:34:52 -0700

    app finds 30 restaurants in PDX

--- application.js
+++ application.js
@@ -1,20 +1,22 @@
 (function() {
   'use strict';
-  var app = (function(){
 
+  var fourSquareUrl = 'https://api.foursquare.com/v2/venues/explore?ll=45.5,-122.7&oauth_token=TAM5U5CGI2BMMZYSGGBCM44MMQWP5TGQ3OJ0A4MIVKLNVTMB&v=20140612&query=restaurant';
 
-    var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=1F2RX5DUSRABEM3QBX4YT5KXH4M0I5TO4F1V2VXGY0OHJEIO&client_secret=JTGCDAVWB4YPHSVT0OMCGW0NQ12JDDPQOLPNAAQDFFLSJ1EO&v=20130815';
-
-    $.ajax(fourSquareUrl, { dataType: 'jsonp', jsonp: 'jsoncallback' })
-      .then(function(data, status, xhr) {
-        console.log(status);
-        console.log('success (promises): ' + data.name);
+  var loadVenues = function() {
+    $.ajax(fourSquareUrl, { dataType: 'jsonp' })
+    .then(function(data, status, xhr) {
+      console.log(status);
+      console.log('success (promises): ');
+      console.log(data);
+      console.log(data.response.groups[0].items[0].venue.name);
     }, function(xhr, status, error) {
       console.log('failed (promises): ' + error);
     });
+  };
 
-
+  $(function() {
+    loadVenues();
   });
-  $(app.run);
 
 })();
