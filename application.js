baa93b542888f5c6873ca8981da9e3fe095a83c7
Ariel Spear <simpleredfox@gmail.com>
2014-06-12 10:16:13 -0700

    Fixed (we think) the fourSquareUrl

--- application.js
+++ application.js
@@ -3,6 +3,16 @@
   var app = (function(){
 
 
+    var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=1F2RX5DUSRABEM3QBX4YT5KXH4M0I5TO4F1V2VXGY0OHJEIO&client_secret=JTGCDAVWB4YPHSVT0OMCGW0NQ12JDDPQOLPNAAQDFFLSJ1EO&v=20130815';
+
+    $.ajax(fourSquareUrl, { dataType: 'jsonp', jsonp: 'jsoncallback' })
+      .then(function(data, status, xhr) {
+        console.log(status);
+        console.log('success (promises): ' + data.name);
+    }, function(xhr, status, error) {
+      console.log('failed (promises): ' + error);
+    });
+
 
   });
   $(app.run);
