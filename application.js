9a057e4a5fc464f9d15303afa8a41e31cbbbc8cb
Ariel Spear <simpleredfox@gmail.com>
2014-06-12 14:15:11 -0700

    Populate HTML, with working menu link if menu exsists, hurray

--- application.js
+++ application.js
@@ -3,6 +3,7 @@
 
   var fourSquareUrl = 'https://api.foursquare.com/v2/venues/explore?ll=45.533,-122.69&oauth_token=TAM5U5CGI2BMMZYSGGBCM44MMQWP5TGQ3OJ0A4MIVKLNVTMB&v=20140612&section=food';
 
+
   var loadVenues = function() {
     $.ajax(fourSquareUrl, { dataType: 'jsonp' })
     .then(function(data, status, xhr) {
@@ -11,14 +12,17 @@
       var venuesArray = data.response.groups[0].items;
       console.log(getVenueInfo(venuesArray));
       console.log(venuesArray);
+      populateHTML(getVenueInfo(venuesArray));
     }, function(xhr, status, error) {
       console.log('failed (promises): ' + error);
     });
   };
 
+
   var getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
+
 
   var getVenueInfo = function(venArray) {
     var randomInt = getRandomInt(0, 29);
@@ -34,8 +38,12 @@
       menuUrl: menuUrl};
   };
 
-  var populateHTML = function(venInfo) {
 
+  var populateHTML = function(venInfo) {
+    $('body').append("<p>" + venInfo.name + "</p><p>" + venInfo.hours + "</p><p>" + venInfo.address + "</p><p>" + venInfo.distance + " meters away.</p>");
+    if (venInfo.menuUrl !== null){
+      $('body').append("<p><a href=" + venInfo.menuUrl + ">Menu</a></p>");
+    }
   };
 
   $(function() {
