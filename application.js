2bf7408ddb05bc3b333b6c54ef6780d6823363ab
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-12 13:35:23 -0700

    Randomly selects one of 30 eateries

--- application.js
+++ application.js
@@ -1,18 +1,38 @@
 (function() {
   'use strict';
 
-  var fourSquareUrl = 'https://api.foursquare.com/v2/venues/explore?ll=45.5,-122.7&oauth_token=TAM5U5CGI2BMMZYSGGBCM44MMQWP5TGQ3OJ0A4MIVKLNVTMB&v=20140612&query=restaurant';
+  var fourSquareUrl = 'https://api.foursquare.com/v2/venues/explore?ll=45.533,-122.69&oauth_token=TAM5U5CGI2BMMZYSGGBCM44MMQWP5TGQ3OJ0A4MIVKLNVTMB&v=20140612&section=food';
 
   var loadVenues = function() {
     $.ajax(fourSquareUrl, { dataType: 'jsonp' })
     .then(function(data, status, xhr) {
       console.log(status);
       console.log('success (promises): ');
-      console.log(data);
-      console.log(data.response.groups[0].items[0].venue.name);
+      var venuesArray = data.response.groups[0].items;
+      console.log(getVenueInfo(venuesArray));
+      console.log(venuesArray);
     }, function(xhr, status, error) {
       console.log('failed (promises): ' + error);
     });
+  };
+
+  var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  var getVenueInfo = function(venArray) {
+    var randomInt = getRandomInt(0, 29);
+    var name = venArray[randomInt].venue.name;
+    var hours = venArray[randomInt].venue.hours.status;
+    var address = venArray[randomInt].venue.location.address;
+    var distance = venArray[randomInt].venue.location.distance;
+    var menuUrl = venArray[randomInt].venue.menu.url;
+    return {name: name, hours: hours, address: address, distance: distance,
+      menuUrl: menuUrl};
+  };
+
+  var populateHTML = function(venInfo) {
+
   };
 
   $(function() {
